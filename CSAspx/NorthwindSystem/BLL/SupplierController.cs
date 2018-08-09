fc47b40ee226b1b4d6910cf34fff1f8a03535f14
sjohnson78 <39070859+sjohnson78@users.noreply.github.com>
2018-08-09 02:06:37 -0600

    csaspx07 - filter search example

--- CSAspx/NorthwindSystem/BLL/SupplierController.cs
+++ CSAspx/NorthwindSystem/BLL/SupplierController.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 #region Additional Namespaces
 using Northwind.Data.Entities;
+using Northwind.Data.Views;
 using NorthwindSystem.DAL;
+using System.Data.SqlClient;
 #endregion
 
 namespace NorthwindSystem.BLL
@@ -26,6 +29,17 @@ namespace NorthwindSystem.BLL
             using (var context = new NorthwindContext())
             {
                 return context.Suppliers.Find(supplierid);
+            }
+        }
+
+        public List<SupplierCategories> Suppliers_GetCategories(int suppilerid)
+        {
+            using (var context = new NorthwindContext())
+            {
+                IEnumerable<SupplierCategories> results =
+                    context.Database.SqlQuery<SupplierCategories>("Suppliers_GetCategories @SupplierID",
+                                    new SqlParameter("SupplierID", suppilerid));
+                return results.ToList();
             }
         }
     }
